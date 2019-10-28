07be7c05e40956936a0480b0cf01bbfc4ae084b0
Oleksandr Danylchenko <o.danylchenko@globallogic.com>
2019-10-28 20:14:46 +0200

    Changes and optimisations

--- celery_django/web_app_celery.py
+++ celery_django/web_app_celery.py
@@ -9,16 +9,25 @@ import django
 
 from celery import Celery
 from kombu import Exchange
+from octo.config_cred import cred
+
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_app.settings')
+if not os.name == "nt":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'octo.settings')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'octo.win_settings')
 # Setup django project
 django.setup()
 # curr_hostname = getattr(settings, 'CURR_HOSTNAME', None)
-#  The backend is specified via the backend argument to Celery, (or via the result_backend setting if you choose to use a configuration module):
-app = Celery('web_app',
-             broker='amqp://mq_user:mq_password@localhost:5672/mq_vhost',
-             backend='amqp://mq_user:mq_password@localhost:5672/mq_vhost',
+#  The backend is specified via the backend argument to Celery,
+#  (or via the result_backend setting if you choose to use a configuration module):
+app = Celery('octo',
+             # http://docs.celeryproject.org/en/latest/userguide/optimizing.html
+             broker=cred['broker'],
+
+             # http://docs.celeryproject.org/en/latest/userguide/configuration.html#result-backend
+             backend=cred['backend'],
              )
 
 # Using a string here means the worker doesn't have to serialize
@@ -26,158 +35,103 @@ app = Celery('web_app',
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 # app.config_from_object('django.conf:settings', namespace='CELERY')
-#  The backend is specified via the backend argument to Celery, (or via the result_backend setting if you choose to use a configuration module):
+#  The backend is specified via the backend argument to Celery,
+#  (or via the result_backend setting if you choose to use a configuration module):
+
+
+app.conf.timezone = 'UTC'
+app.conf.enable_utc = True
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
+# One for internal:
+default_exchange = Exchange('default', type='direct')
+# One for tests only
+tests_exchange = Exchange('tests_run', type='direct', durable=False)
+
+app.conf.update(
+    accept_content=['json', 'pickle', 'application/x-python', 'application/json', 'application/x-python-serialize'],
+    task_serializer='pickle',
+    result_serializer='json',
+    # Do not set! Or logic will not wait of task OK: # task_ignore_result=True,
+    task_track_started = True,
+
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#result-backend
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#database-url-examples
+    # https://docs.celeryproject.org/en/latest/getting-started/first-steps-with-celery.html#keeping-results
+    # 1406, "Data too long for column 'result' at row 1" - it's not so important to keep it in DB
+    result_backend = cred['result_backend'],
+
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#beat-scheduler
+    beat_scheduler='django_celery_beat.schedulers:DatabaseScheduler',
+
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-default-queue
+    task_default_queue='default',
+    task_default_exchange='tests_run',
+    task_default_routing_key='default',
+    task_default_exchange_type='direct',
+
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-create-missing-queues
+    # task_create_missing_queues=True,  # By Default
+
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#worker-prefetch-multiplier
+    worker_prefetch_multiplier=0,
+
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#events
+    # worker_send_task_events=True,  # -E at worker service
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-send-sent-event
+    # task_send_sent_event=True,
+    worker_pool_restarts=True,
+
+    # Maybe this should be disabled to allow workers to get ALL tasks in queue.
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_acks_late
+    # http://docs.celeryproject.org/en/master/faq.html#faq-acks-late-vs-retry
+    # task_acks_late = False,  # By Default
+
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#task-track-started
+    # task_track_started=True,
+
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#broker-pool-limit
+    broker_pool_limit=None,
+
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#broker-connection-timeout
+    broker_connection_timeout=2,
+    broker_connection_retry=True,
+    broker_connection_max_retries=0,
+
+    # https://github.com/celery/celery/issues/5340
+    # http://docs.celeryproject.org/en/latest/userguide/routing.html#routing-options-rabbitmq-priorities
+    # task_queue_max_priority  = 10,
+    # task_default_priority = 5,
+
+    # Dev IDEA:
+    # http://docs.celeryproject.org/en/latest/userguide/configuration.html#worker-direct
+    worker_direct = True,
+
+)
 
 if not os.name == "nt":
-    # https://github.com/celery/celery/issues/4184
-    app.conf.timezone   = 'UTC'
-    app.conf.enable_utc   = True
-
-    # Load task modules from all registered Django app configs.
-    app.autodiscover_tasks()
-
-    # One for internal:
-    default_exchange = Exchange('default',   type = 'direct')
-    # One for tests only
-    tests_exchange   = Exchange('tests_run', type = 'direct')
-
-    app.conf.update(
-        accept_content    = ['json', 'pickle', 'application/x-python', 'application/json', 'application/x-python-serialize'],
-        task_serializer   = 'pickle',
-        result_serializer = 'json',
-        # Do not set! Or logic will not wait of task OK: # task_ignore_result=True,
-        # http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html#django-celery-results-using-the-django-orm-cache-as-a-result-backend
-        result_backend = 'db+mysql://celery_backend:celery_backend_passwd@localhost/web_app_database',
-
-
-        # result_backend = 'django-db',  # Use if 'django_celery_results'
-        # INFO: This added as arg to celery service in Centos init.d | BUT should be used here?
-        beat_scheduler             = 'django_celery_beat.schedulers:DatabaseScheduler',
-        task_default_queue         = 'default',
-        task_default_exchange      = 'default',
-        task_default_routing_key   = 'default',
-        task_default_exchange_type = 'direct',
-        task_create_missing_queues = True,
-
-        # http://docs.celeryproject.org/en/master/userguide/configuration.html#std:setting-worker_prefetch_multiplier
-        worker_prefetch_multiplier = 0,
-        # worker_prefetch_multiplier = 2000,
-
-        # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-worker_send_task_events
-        worker_send_task_events = True,
-        task_send_sent_event    = True,
-        worker_pool_restarts    = True,
-
-        # Maybe this should be disabled to allow workers to get ALL tasks in queue.
-        # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_acks_late
-        # http://docs.celeryproject.org/en/master/faq.html#faq-acks-late-vs-retry
-        # task_acks_late = False,
-
-        # Experimental:
-        # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-result_persistent
-        # result_persistent = True,
-        # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-task_track_started
-        task_track_started = True,
-
-        # Can produce web errors: [Errno 104] Connection reset by peer???
-        # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_pool_limit
-        # https://stackoverflow.com/questions/45803728/celery-upgrade-3-1-4-1-connection-reset-by-peer
-        broker_pool_limit = None,  # Keep connection always up!
-        # http://docs.celeryproject.org/en/latest/userguide/configuration.html#broker-connection-timeout
-        # broker_connection_timeout     = 2,
-        # broker_connection_retry       = True,
-        # broker_connection_max_retries = 0,
-        # http://docs.celeryproject.org/en/latest/userguide/configuration.html#std:setting-broker_transport_options
-    )
-
-    app.control.add_consumer(
-        queue='perforce',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'perforce.*',
-        destination = ['w_perforce@mq_vhost'])
-
-    # Keep parsing with default queue for local and system tasks only:
-    app.control.add_consumer(
-        queue='parsing',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'parsing.*',
-        destination = ['w_parsing@mq_vhost'])
-
-    app.control.add_consumer(
-        queue='routines',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'routines.*',
-        destination = ['w_routines@mq_vhost'])
-
-    # New:
-    app.control.add_consumer(
-        queue='alpha',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'alpha.*',
-        destination = ['alpha@mq_vhost'])
-
-    app.control.add_consumer(
-        queue='beta',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'beta.*',
-        destination = ['beta@mq_vhost'])
-
-    app.control.add_consumer(
-        queue='charlie',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'charlie.*',
-        destination = ['charlie@mq_vhost'])
-
-    app.control.add_consumer(
-        queue='delta',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'delta.*',
-        destination = ['delta@mq_vhost'])
-
-    app.control.add_consumer(
-        queue='echo',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'echo.*',
-        destination = ['echo@mq_vhost'])
-
-    app.control.add_consumer(
-        queue='foxtrot',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'foxtrot.*',
-        destination = ['foxtrot@mq_vhost'])
-
-    # AGAIN! I SAID ADD CONSUMER!
-    app.control.add_consumer(
-        queue='development',
-        exchange = 'tests_run',
-        exchange_type = 'direct',
-        routing_key   = 'development.*',
-        destination = ['development@mq_vhost'])
-
     app.control.cancel_consumer(
         'default',
-        destination = [
-            'w_perforce@mq_vhost',
-            'w_routines@mq_vhost',
-            'alpha@mq_vhost',
-            'beta@mq_vhost',
-            'charlie@mq_vhost',
-            'delta@mq_vhost',
-            'echo@mq_vhost',
-            'foxtrot@mq_vhost',
-            'development@mq_vhost',
+        destination=[
+            'w_routines@tentacle',
+            'alpha@tentacle',
+            'beta@tentacle',
+            'charlie@tentacle',
+            'delta@tentacle',
+            'echo@tentacle',
+            'foxtrot@tentacle',
+            'w_development@tentacle',
         ])
-
-# To activate all queues and tasks are left:
-# Or better use proper restart/reloas.
-# app.control.inspect()
-# app.control.inspect().active_queues()
+else:
+    app.control.cancel_consumer(
+        'default',
+        destination=[
+            'w_routines@tentacle',
+            'alpha@tentacle',
+            # 'beta@tentacle',
+            'charlie@tentacle',
+            # 'delta@tentacle',
+            # 'echo@tentacle',
+            # 'foxtrot@tentacle',
+            # 'w_development@tentacle',
+        ])
---
+++ celery_services/windows/alpha.bat
@@ -0,0 +1,5 @@
+D:\Projects\PycharmProjects\lobster\venv\Scripts\activate.bat && celery -A octo.octo_celery:app worker --pool=eventlet --loglevel=ERROR --concurrency=1 -E -n alpha@tentacle
+# Path D:\Projects\PycharmProjects\lobster\z_DEV\services\celery_services\windows\proj_H\alpha.bat
+# Startup directory: D:\Projects\PycharmProjects\lobster\
+# Output: D:\Projects\PycharmProjects\lobster\z_DEV\services\celery_services\windows\logs\alpha_out.log
+# Error: D:\Projects\PycharmProjects\lobster\z_DEV\services\celery_services\windows\logs\alpha_err.log
