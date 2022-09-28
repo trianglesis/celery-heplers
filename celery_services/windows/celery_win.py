3dd79c211c5493d618a87518198b816716ce91cf
Oleksandr Danylchenko <sanekm2010@gmail.com>
2022-09-28 16:38:43 +0300

    Update celery_win.py
    Even better killing celery processes.

--- celery_services/windows/celery_win.py
+++ celery_services/windows/celery_win.py
@@ -2,9 +2,11 @@ import argparse
 import os
 import subprocess
 import atexit
+import signal
+import sys
+from multiprocessing import Queue
 from multiprocessing.pool import ThreadPool
 from time import time
-
 
 """
 Setup here your:
@@ -50,10 +52,6 @@ workers = [
 ]
 
 
-def kill_runner(runner):
-    runner.terminate()
-
-
 def th_run(args):
     print(args)
     mode = args.mode
@@ -75,74 +73,123 @@ def th_run(args):
         if log_level == 'DEBUG':
             print(f'<==Celery WIN==> Will wipe all previously generated logs.')
             for worker in worker_list:
-                os.remove(f'{LOG_PATH}{os.sep}{worker}.log')
+                log_f = f'{LOG_PATH}{os.sep}{worker}.log'
+                if os.path.exists(log_f):
+                    os.remove(log_f)
 
-    cmd_list = []
+    cmd_dict = dict()
     # Start or stop or kill list of workers:
     if mode == 'start':
         for worker in worker_list:
             cmd_f = f"{ENV} && celery -A {APP} worker --loglevel={log_level} --concurrency=1 -E -n {worker}@tentacle --logfile={LOG_PATH}{os.sep}{worker}.log"
-            cmd_list.append(cmd_f)
+            cmd_dict.update({worker: cmd_f})
 
     # Only start beat as separated process:
     # win_venv\Scripts\activate.bat && celery beat --schedule=django_celery_beat.schedulers:DatabaseScheduler
     elif mode == 'beat':
         cmd_f = f"{ENV} && celery -A {APP} beat --loglevel={log_level} --logfile={LOG_PATH}{os.sep}beat.log {SCHEDULE}"
-        cmd_list.append(cmd_f)
+        cmd_dict.update({"beat": cmd_f})
 
     else:
         print(f"<==Celery WIN==> MODE is not supported: {mode}")
 
     if log_level == 'DEBUG':
-        print(f"<==Celery WIN==> Composed command list: {cmd_list}")
+        print(f"<==Celery WIN==> Composed command list:")
+        for worker, cmd in cmd_dict.items():
+            print(f'{worker}: "{cmd}" ')
 
+    # Execute:
+    multi(cmd_dict)
+
+
+def multi(cmd_dict):
     # Now run commands each in separate thread
     ts = time()
+    outputs = []
     # As many as workers we have
-    pool = ThreadPool(processes=len(cmd_list))
+    print(f"<==Celery WIN==> Process pools: {len(cmd_dict)}")
+    pool = ThreadPool(processes=len(cmd_dict))
+    queue = Queue()
 
     def kill_pool(err_msg):
         print(err_msg)
         pool.terminate()
 
-    for cmd in cmd_list:
-        if log_level == 'DEBUG':
-            print(f"<==Celery WIN==> Run: {cmd}")
-
+    for worker, cmd in cmd_dict.items():
+        print(f"<==Celery WIN==> Run pool.apply_async: {cmd}")
         pool.apply_async(workers_run,
-                         kwds=dict(cmd=cmd, cwd=os.path.dirname(os.path.realpath(__file__))),
+                         kwds=dict(queue=queue, cmd=cmd, worker=worker),
                          error_callback=kill_pool)
+    try:
+        pool.close()
+        pool.join()
+    except KeyboardInterrupt as e:
+        out = [queue.get() for i in range(len(cmd_dict))]
+        for o in out:
+            outputs.append(o)
 
-    pool.close()
-    pool.join()
-    pool.terminate()
+        # Any other finish condition?
+        pool.terminate()
 
-    atexit.register(kill_runner, workers_run)
-    exit()
+        print(f"<==Celery WIN Exit==> Keyboard interrupt - kill pool!"
+              f"\n\tWorked for: {time() - ts}"
+              f"\n\tCommand out: {outputs}"
+              f"\n\tException: {e}")
 
-    print(f'All run Took {time() - ts}')
+    print(f'Finally: All run Took {time() - ts}')
+    return None
 
 
 def workers_run(**kwargs):
+    queue = kwargs.get('queue')
     cmd = kwargs.get('cmd')
-    cwd = kwargs.get('cwd')
+    worker = kwargs.get('worker')
+
+    cwd = os.path.dirname(os.path.realpath(__file__))
     my_env = os.environ.copy()
-    run_results = []
+    run_cmd = None
+
     try:
         run_cmd = subprocess.Popen(cmd,
-                                   stdout=subprocess.PIPE,
+                                   stdout=subprocess.PIPE,  # Redirect, but do not show
                                    stderr=subprocess.PIPE,
                                    cwd=cwd,
                                    env=my_env,
                                    shell=True,
                                    )
-        # run_cmd.wait()
-        stdout, stderr = run_cmd.communicate()
-        stdout, stderr = stdout.decode('utf-8'), stderr.decode('utf-8')
-        run_results.append({'stdout': stdout, 'stderr': stderr})
+
+        _, stderr = run_cmd.communicate()
+        stderr = stderr.decode('utf-8')
+
+        if stderr and 'Hitting Ctrl+C again' in stderr:
+            print(f"<==Celery WIN Exit==> '{worker}' is asking for Ctrl+C! Sending: signal.CTRL_C_EVENT")
+            os.kill(run_cmd.pid, signal.CTRL_C_EVENT)
+
+            print(f"<==Celery WIN Exit==> '{worker}' Ensure killing process with: signal.SIGINT and complete kill")
+            run_cmd.send_signal(signal.SIGINT)
+            run_cmd.kill()
+            queue.put(f"MSG: '{worker}' Killed Celery worker on Keyboard interrupt.")
+
+    except KeyboardInterrupt:
+        print(f"<==Celery WIN Exit==> '{worker}' Keyboard interrupt - kill process")
+        os.kill(run_cmd.pid, signal.CTRL_C_EVENT)
+        run_cmd.send_signal(signal.SIGINT)
+        run_cmd.kill()
+
+        queue.put(f"'{worker}' - Keyboard interrupt - kill worker, Exception pass.")
+        pass
+
     except Exception as e:
-        msg = f"<==Celery WIN==> Error during operation for: {cmd} {e}"
-        print(msg)
+        print(f"<==Celery WIN Exit==> '{worker}' Error during operation: {e}")
+
+        os.kill(run_cmd.pid, signal.CTRL_C_EVENT)
+        run_cmd.send_signal(signal.SIGINT)
+        run_cmd.kill()
+
+        queue.put(f"{worker} - Error, kill worker")
+        pass
+
+    return None
 
 
 if __name__ == '__main__':
@@ -152,7 +199,6 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--log_level', choices=['DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'])
     parser.add_argument('--wipe_logs', action='store_true', help='Wipe previously generated logs content')
     th_run(parser.parse_args())
-
 
 """
 This is the simplest script for celery workers run on windows. Nothing else.
@@ -164,10 +210,10 @@ python celery_win.py --mode=beat
 Start ALL workers available:
 python celery_win.py --mode=start --log_level=DEBUG --wipe_logs
 python celery_win.py --mode=start --log_level=DEBUG
-python celery_win.py --mode=start --log_level=INFO
+python celery_win.py --mode=start --log_level=INFO --wipe_logs
 
 Start only few workers:
 python celery_win.py --mode=start --worker=alpha --log_level=DEBUG
-python celery_win.py --mode=start --worker=alpha --worker=beta --log_level=DEBUG
+python celery_win.py --mode=start --worker=alpha --worker=beta --log_level=DEBUG --wipe_logs
 
 """
